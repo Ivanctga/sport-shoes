a7d83075fc96f9ffd6f0766636be30b443a0c2f4
Ivanctga <123258403+Ivanctga@users.noreply.github.com>
2024-02-01 13:58:35 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,79 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+let slideIndex = 0
+
+let slider = document.getElementById('slider')
+
+let slides = slider.getElementsByClassName('slide')
+
+let slideControl = document.getElementById('slide-control')
+
+let slideControlItems = slideControl.getElementsByClassName('slide-item')
+
+setTimeout(() => {
+  slider.style.marginTop = "-" + slideIndex + "00vh";
+  slideControlItems[slideIndex].classList.add("active");
+  slides[slideIndex].classList.add("active");
+}, 500);
+
+chooseProduct = (index) => {
+    if (index === slideIndex) return
+
+    slideIndex = index
+
+    let currSlideControl = slideControl.querySelector
+    ('.slide-item.active')
+    currSlideControl.classList.remove('active')
+
+    let currSlide = slider.querySelector('.slide.active')
+    currSlide.classList.remove("active");    
+
+    setTimeout(() => {
+        slider.style.marginTop = "-" + slideIndex + "00vh";
+        slideControlItems[slideIndex].classList.add("active");
+        slides[slideIndex].classList.add("active");
+    }, 1500)
+}
+
+Array.from(slideControlItems).forEach((el, index) => {
+    el.onclick = function() {
+        chooseProduct(index);
+    }
+})
+
+let modal =document.getElementById('modal')
+
+let closeBtn = document.getElementById('modal-close')
+
+closeBtn.onclick = () => {
+    modal.style.display = 'none'
+}
+
+let moreImages = document.getElementsByClassName("row-img-items");
+
+let previewImages = document.getElementsByClassName('img-preview');
+
+Array.from(moreImages).forEach((el) => {
+  el.onclick = () => {
+    let imgItems = el.parentNode.getElementsByTagName('img')
+
+    Array.from(imgItems).forEach((item, index) => {
+        previewImages[index].src = item.src
+    }) 
+
+    let img = el.querySelector('img')
+    modal.style.display = 'block'
+
+    let modalContent = modal.querySelector('.modal-content')
+    modalContent.src = img.src
+
+    let temp = modalContent.cloneNode(true)
+    modalContent.parentNode.replaceChild(temp, modalContent)
+  }
+})
